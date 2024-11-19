f44dd98eb10188470dc44475eeb986a6136d9c8b
Mateus Dev <mateusdev23@gmail.com>
2024-11-19 11:16:07 -0300

    Exercicio 01

---
+++ Exercicio01/script.js
@@ -0,0 +1,9 @@
+/* ::: Exercicio 01 :::
+
+Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+*/
+
+const mensagem = document.getElementById("mensagem");
+mensagem.innerHTML = "Olá mundo!";
+
+// document.write("Olá mundo");
