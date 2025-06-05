e9d3e52e6c2c8724bed684bd9e676171f35db7aa
Mateusinhodev <mateusdev23@gmail.com>
2025-06-05 13:11:38 -0300

    Refazendo o Exercicio 01, criando um letreiro digital

--- ex01/script.js
+++ ex01/script.js
@@ -3,7 +3,7 @@
 Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
 */
 
-const mensagem = document.getElementById("mensagem");
+const mensagem = document.getElementById("textoletreiro");
 mensagem.innerHTML = "Olá mundo!";
 
 // document.write("Olá mundo");
